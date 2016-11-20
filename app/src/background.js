4fb0bc8533d446ebd426a81da6f0ef4cc9959e6e
Tristan <thrawst88@gmail.com>
2016-11-20 01:02:43 -0700

    fixed some errors - wouldn't actually download podcast - extension title would not properly update - version number bump

--- app/src/background.js
+++ app/src/background.js
@@ -1,23 +1,27 @@
-
 // Disable by default
+var disabledString = 'Only enabled on the Overcast.fm webplayer';
 chrome.browserAction.disable();
-chrome.browserAction.setTitle({ title: 'Only enabled on Overcast podcast page' });
+chrome.browserAction.setTitle({ title: disabledString });
 
+// for when url changes
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf('overcast.fm/+') !== -1) {
-    chrome.browserAction.enable(tabId);
-    chrome.browserAction.setTitle({ title: 'Download current podcast' });
-  } else {
-    chrome.browserAction.disable(tabId);
-    chrome.browserAction.setTitle({ title: 'Only enabled on Overcast podcast page' });
+  if (changeInfo.status === 'complete') {
+    if (tab.url && tab.url.indexOf('overcast.fm/+') !== -1) {
+      chrome.browserAction.enable(tabId);
+      chrome.browserAction.setTitle({ title: 'Download current podcast', tabId: tabId });
+    } else {
+      chrome.browserAction.disable(tabId);
+      chrome.browserAction.setTitle({ title: disabledString, tabId: tabId });
+    }
   }
 });
 
 chrome.browserAction.onClicked.addListener(function(info, tab) {
-  chrome.tabs.executeScript(null, { file: "downloader.js" }, function() {
+  // { file: ... } takes relative path to manifest
+  chrome.tabs.executeScript(null, { file: 'src/downloader.js' }, function(result) {
     if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error running the download script : \n' + 
-        chrome.runtime.lastError.message;
+      console.error('There was an error running the download script : \n' + 
+        chrome.runtime.lastError.message);
     }
   });
 });
