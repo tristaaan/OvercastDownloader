80d4c0d8dae12a71299303eddec3ad154b9794b2
Tristan <thrawst88@gmail.com>
2016-11-19 23:53:52 -0700

    updated manifest, file structure

--- app/src/background.js
+++ app/src/background.js
@@ -16,7 +16,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 chrome.browserAction.onClicked.addListener(function(info, tab) {
   chrome.tabs.executeScript(null, { file: "downloader.js" }, function() {
     if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      message.innerText = 'There was an error running the download script : \n' + 
+        chrome.runtime.lastError.message;
     }
   });
 });
